ddd59d629a10a7e8ddd936576265696127eb2ec3
vikram-s-sigmoid <vikrams@sigmoidanalytics.com>
2025-03-05 02:18:30 +0530

    Added API endpoint

--- src/apiCalls/constants.js
+++ src/apiCalls/constants.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "http://127.0.0.1:8000/api"; // Change when deployed
+// export const API_BASE_URL = "http://127.0.0.1:8000/api"; // Change when deployed
+export const API_BASE_URL = "https://fithub-app-be.onrender.com/api";
 
 export const API_ENDPOINTS = {
   PRODUCTS: "/products",
